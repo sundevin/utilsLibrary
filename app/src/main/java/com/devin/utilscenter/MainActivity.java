fd7588f7574bec92557f6e15818c0c36028f8533
孙德武 <bjxm2013@163.com>
2023-10-21 15:50:57 +0800

    更新demo

--- app/src/main/java/com/devin/utilscenter/MainActivity.java
+++ app/src/main/java/com/devin/utilscenter/MainActivity.java
@@ -2,9 +2,10 @@ package com.devin.utilscenter;
 
 import android.graphics.Color;
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
 import android.widget.ImageView;
 import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 import com.devin.util.ImageLoader;
 
